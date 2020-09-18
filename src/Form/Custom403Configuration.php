1a8f6076befff68647697f0896c4f40c721ca54e
Alison Jo <2660348+alisonjo315@users.noreply.github.com>
2020-09-17 20:13:44 -0400

    admin form help text and label updates

--- src/Form/Custom403Configuration.php
+++ src/Form/Custom403Configuration.php
@@ -34,7 +34,7 @@ class Custom403Configuration extends ConfigFormBase {
     $form['403_custom_text'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Text for 403 page'),
-      '#description' => $this->t('HTML is permitted; shown under page title ("Access denied").'),
+      '#description' => $this->t('HTML is permitted; shown under page title ("Access denied"). Default: "Restricted access - please login to continue."'),
       '#default_value' => $config->get('403_custom_text'),
       '#size' => 200,
       '#maxlength' => 255,
@@ -47,12 +47,12 @@ class Custom403Configuration extends ConfigFormBase {
     ];
     $form['login_buttons']['403_use_cornell'] =[
       '#type' => 'checkbox',
-      '#title' => $this->t('Site uses Cornell SSO login'),
+      '#title' => $this->t('Include Cornell SSO login button'),
       '#default_value' => $config->get('403_use_cornell'),
     ];
     $form['login_buttons']['403_use_drupal'] =[
       '#type' => 'checkbox',
-      '#title' => $this->t('Site uses Drupal login'),
+      '#title' => $this->t('Include Drupal login button'),
       '#default_value' => $config->get('403_use_drupal'),
     ];
     return parent::buildForm($form, $form_state);
